0fd05ca32eab6299128cbdcc577f2457df843b39
Sâmara Jeise Dias <samarajeise@hotmail.com>
2025-08-05 14:32:29 -0300

    Criando Rotas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
   return (
     <>
@@ -5,7 +7,7 @@ function Navbar() {
             			    bg-indigo-900 text-white'>
 
         <div className="container flex justify-between text-lg">
-          BioFarma
+          <Link to='/home' className="text-2xl font-bold">BioFarma</Link>
 
           <div className='flex gap-4'>
             Categoria
