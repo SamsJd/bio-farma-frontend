68133812740492b12edc4e88a78a14e8a3bbbdf4
Sâmara Jeise Dias <samarajeise@hotmail.com>
2025-08-05 22:37:32 -0300

    Update Componente ListaCategorias

--- src/components/categorias/listacategorias/ListaCategorias.tsx
+++ src/components/categorias/listacategorias/ListaCategorias.tsx
@@ -1,22 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CardCategorias from "../cardcategorias/CardCategorias";
 import type Categoria from "../../../models/Categoria";
-import { useNavigate } from "react-router-dom";
-
+import { buscar } from "../../../services/Service";
+import { DNA } from "react-loader-spinner";
 
 function ListaCategorias() {
 
-  const navigate = useNavigate()
+  const [categorias, setCategorias] = useState<Categoria[]>([])
 
-    const [categorias, setCategorias] = useState<Categoria[]>([])
+  async function buscarCategorias() {
+    try {
+      await buscar("/categorias", setCategorias, {});
+    } catch (error) {
+      console.error("Erro ao buscar categorias:", error);
+    }
+  }
+
+  useEffect(() => {
+    buscarCategorias();
+  }, [categorias.length]);
 
   return (
     <>
+      {categorias.length === 0 && (
+        <DNA
+          visible={true}
+          height="200"
+          width="200"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper mx-auto"
+        />
+      )}
       <div className="flex justify-center w-full my-4">
         <div className="container flex flex-col">
           <div className="grid grid-cols-1 md:grid-cols-2 
                           lg:grid-cols-3 gap-8">
-            <CardCategorias />
+            {categorias.map((categoria) => (
+              <CardCategorias key={categoria.id} categoria={categoria} />
+            ))}
           </div>
         </div>
       </div>
