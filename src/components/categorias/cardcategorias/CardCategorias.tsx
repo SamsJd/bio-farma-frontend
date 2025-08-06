270396c5d17831d65caf0845f61ec5791f7d03bf
Sâmara Jeise Dias <samarajeise@hotmail.com>
2025-08-05 23:36:09 -0300

    Criando Componente Formulário Categoria - Lógica

--- src/components/categorias/cardcategorias/CardCategorias.tsx
+++ src/components/categorias/cardcategorias/CardCategorias.tsx
@@ -14,8 +14,8 @@ function CardCategorias({ categoria }: CardCategoriasProps) {
       <p className='p-8 text-3xl bg-slate-200 h-full'>{categoria.descricao}</p>
 
       <div className="flex">
-        <Link to='' 
-          className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 
+        <Link to={`/editarcategoria/${categoria.id}`}
+          className='w-full text-slate-100 bg-	indigo-400 hover:bg-indigo-800 
             flex items-center justify-center py-2'>
           <button>Editar</button>
         </Link>
--- src/components/categorias/formcategoria/FormCategoria.tsx
+++ src/components/categorias/formcategoria/FormCategoria.tsx
@@ -1,29 +1,103 @@
-function FormCategoria() {
-    return (
-        <div className="container flex flex-col items-center justify-center mx-auto">
-            <h1 className="text-4xl text-center my-8">
-                Cadastrar Categoria
-            </h1>
+import { useEffect, useState, type ChangeEvent, type FormEvent } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import type Categoria from "../../../models/Categoria";
+import { atualizar, buscar, cadastrar } from "../../../services/Service";
+import { RotatingLines } from "react-loader-spinner";
 
-            <form className="w-1/2 flex flex-col gap-4" >
-                <div className="flex flex-col gap-2">
-                    <label htmlFor="descricao">Descrição da Categoria</label>
-                    <input
-                        type="text"
-                        placeholder="Descreva aqui sua categoria"
-                        name='descricao'
-                        className="border-2 border-slate-700 rounded p-2"
-                    />
-                </div>
-                <button
-                    className="rounded text-slate-100 bg-indigo-400 
-                              hover:bg-indigo-800 w-1/2 py-2 mx-auto flex justify-center"
-                    type="submit">
-                    Cadastrar
-                </button>
-            </form>
+function FormCategoria() {
+
+  const navigate = useNavigate();
+  const { id } = useParams<{ id: string }>();
+
+  const [categoria, setCategoria] = useState<Categoria>({
+    id: 0,
+    nome: "",
+    descricao: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function buscarPorId(id: string) {
+    try {
+      await buscar(`/categorias/${id}`, setCategoria , {});
+    } catch (error) {
+      console.error("Erro ao buscar categoria por ID:", error);
+    }
+  }
+
+  useEffect(() => {
+    if (id) {
+      buscarPorId(id);
+    }
+  }, [id]);
+
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setCategoria({
+      ...categoria,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function retornar() {
+    navigate("/categorias");
+  }
+
+  async function gerarNovaCategoria(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      if (id) {
+        await atualizar(`/categorias`, categoria, setCategoria, {});
+        console.log("Categoria atualizada com sucesso.");
+      } else {
+        await cadastrar(`/categorias`, categoria, setCategoria, {});
+        console.log("Categoria cadastrada com sucesso.");
+      }
+    } catch (error) {
+      console.error("Erro ao salvar categoria:", error);
+    }
+
+    setIsLoading(false);
+    retornar();
+  }
+return (
+    <div className="container flex flex-col items-center justify-center mx-auto">
+      <h1 className="text-4xl text-center my-8">
+        {id === undefined ? 'Cadastrar Categoria' : 'Editar Categoria'}
+      </h1>
+
+      <form className="w-1/2 flex flex-col gap-4" onSubmit={gerarNovaCategoria}>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="descricao">Descrição do Categoria</label>
+          <input
+            type="text"
+            placeholder="Descreva aqui seu categoria"
+            name='descricao'
+            className="border-2 border-slate-700 rounded p-2"
+            value={categoria.descricao}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+          />
         </div>
-    );
+        <button
+          className="rounded text-slate-100 bg-indigo-400 
+                    hover:bg-indigo-800 w-1/2 py-2 mx-auto flex justify-center"
+          type="submit">
+          {isLoading ?
+            <RotatingLines
+              strokeColor="white"
+              strokeWidth="5"
+              animationDuration="0.75"
+              width="24"
+              visible={true}
+            /> :
+            <span>{id === undefined ? 'Cadastrar' : 'Atualizar'}</span>
+
+          }
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default FormCategoria;
